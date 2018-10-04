d4e30817136ce6917e67acd6dfdd5f0d34ef1b53
codesahib <gursahib.kvm@gmail.com>
2018-10-04 09:21:25 +0530

    second comit

--- app/src/main/java/gursahibsingh/workoutassistant/BackActivity.java
+++ app/src/main/java/gursahibsingh/workoutassistant/BackActivity.java
@@ -11,5 +11,5 @@ public class BackActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_back);
     }
-    //Functionality Here
+    //Functionality Here.
 }
