c77f3cbc7aed18d9c72a2a6feab176df3ca5288c
codesahib <gursahib.kvm@gmail.com>
2018-10-04 09:20:45 +0530

    second comit

--- app/src/main/java/gursahibsingh/workoutassistant/BackActivity.java
+++ app/src/main/java/gursahibsingh/workoutassistant/BackActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class BackActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_back);
     }
+    //Functionality Here
 }
